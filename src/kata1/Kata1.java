5d59406016807962d410f569660704b6c094eae6
Usuario <Usuario@10.22.148.95>
2018-09-13 13:24:47 +0100

    Second version of first exercise

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,21 @@
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Kata1 {
      
         public static void main (String [] args){
-            Person person = new Person ("Blas", new Date(49,8,24));
-            System.out.println(person.getName() + " tiene " + person.getAge());
-    }
+            Calendar date;
+            date = GregorianCalendar.getInstance();
+            date.set(1949,9,24);
+           
+            Person person = new Person ("Blas", date);
+            System.out.println(person.getName() + " tiene " 
+                    + person.getAge() + " años ");
+        
+        
+        }
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,12 +1,13 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthdate;
-    
-    public Person (String name, Date birthdate){
+    private final Calendar birthdate;
+    private final long MILLISECODS_PER_YEAR = (long) (1000*60*60*24*365.25);
+    public Person (String name, Calendar birthdate){
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -15,13 +16,17 @@ public class Person {
     return name;    
     }
 
-        public Date getBirthday() {
+        public Calendar getBirthday() {
             return birthdate;
         
         }
         
         public int getAge(){
-            return (int) ((new Date().getTime() - birthdate.getTime())/31536000000L);
+            Calendar today = GregorianCalendar.getInstance();
+            return (int) (milliSecondsToYear(today.getTimeInMillis()-birthdate.getTimeInMillis()));
         }
-    
+        
+        private long milliSecondsToYear (long milles){
+            return milles/MILLISECODS_PER_YEAR;
+        }
 }
